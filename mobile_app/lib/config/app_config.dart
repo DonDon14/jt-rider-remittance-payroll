575ee026d91d0954148a5c91fb6d4b042f079f02
DonDon14 <floroocero18@gmail.com>
2026-04-08 08:21:05 +0800

    Point mobile app to deployed subfolder API path

--- mobile_app/lib/config/app_config.dart
+++ mobile_app/lib/config/app_config.dart
@@ -2,8 +2,9 @@ class AppConfig {
   const AppConfig._();
 
   // Change this to the reachable backend URL before running the mobile app.
-  static const String apiBaseUrl = 'http://192.168.18.74:8080/api';
+  static const String apiBaseUrl = 'http://192.168.18.74/J%26T%20Remmittance%20and%20Payroll%20App/public/api';
   static const String appTitle = 'J&T Rider Portal';
 }
+
 
 
